6c042a31546ebd5c9d0e91d9840cdc58631dd9f7
Fahim <fahimsafaat67@gmail.com>
2025-05-27 21:02:43 +0600

    all done

--- CodeMama_Ostad_moudle_problems/Module_11/6.Fibonacci.dart
+++ CodeMama_Ostad_moudle_problems/Module_11/6.Fibonacci.dart
@@ -1,2 +1,26 @@
 import 'dart:io';
 
+void main() {
+  const int mod = 1000000007;
+  int n = int.parse(stdin.readLineSync()!);
+
+  if (n == 0) {
+    print(0);
+    return;
+  } else if (n == 1) {
+    print(1);
+    return;
+  }
+
+  int a = 0;
+  int b = 1;
+  int fib = 0;
+
+  for (int i = 2; i <= n; i++) {
+    fib = (a + b) % mod;
+    a = b;
+    b = fib;
+  }
+
+  print(fib);
+}
